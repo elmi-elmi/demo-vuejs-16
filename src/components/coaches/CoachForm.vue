1a9a814261f6ecbe5a35ef396a65663210f42a67
elmi <shahrokhelmi@gmail.com>
2021-09-24 15:45:38 +0330

    video 15

---
+++ src/components/coaches/CoachForm.vue
@@ -0,0 +1,124 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div class="form-control">
+      <label for="firstname"> first Name</label>
+      <input type="text" id="firstname" v-model.trim="firstName" />
+    </div>
+    <div class="form-control">
+      <label for="lastname">Last Name</label>
+      <input type="text" id="lastname" v-model.trim="lastName" />
+    </div>
+    <div class="form-control">
+      <label for="description">description</label>
+      <textarea
+        name="description"
+        id="description"
+        rows="5"
+        v-model="description"
+      ></textarea>
+    </div>
+    <div class="form-control">
+      <label for="hourlyrate">Hourly Rate</label>
+      <input type="number" id="hourlyrate" v-model.number="rate" />
+    </div>
+    <div class="form-control">
+      <h3>Areas of Expertise</h3>
+      <div>
+        <div>
+          <input type="checkbox" id="frontend" v-model="areas" />
+          <label for="frontend">Frontend</label>
+        </div>
+        <div>
+          <input type="checkbox" id="backend" v-model="areas" />
+          <label for="backend">Backend</label>
+        </div>
+        <div>
+          <input type="checkbox" id="career" v-model="areas" />
+          <label for="career">Career</label>
+        </div>
+      </div>
+    </div>
+    <base-button>Register</base-button>
+  </form>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      description: '',
+      rate: null,
+      areas: []
+    };
+  },
+  methods: {
+    submitForm() {
+      const dataForm = {
+        firsName: this.firstName,
+        lastName: this.lastName,
+        description: this.description,
+        rate: this.rate,
+        area: this.areas
+      };
+      console.log(dataForm);
+    }
+  }
+};
+</script>
+<style scoped>
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input[type='checkbox'] + label {
+  font-weight: normal;
+  display: inline;
+  margin: 0 0 0 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  border: 1px solid #ccc;
+  font: inherit;
+}
+
+input:focus,
+textarea:focus {
+  background-color: #f0e6fd;
+  outline: none;
+  border-color: #3d008d;
+}
+
+input[type='checkbox'] {
+  display: inline;
+  width: auto;
+  border: none;
+}
+
+input[type='checkbox']:focus {
+  outline: #3d008d solid 1px;
+}
+
+h3 {
+  margin: 0.5rem 0;
+  font-size: 1rem;
+}
+
+.invalid label {
+  color: red;
+}
+
+.invalid input,
+.invalid textarea {
+  border: 1px solid red;
+}
+</style>
--- src/pages/coaches/CoachRegestration.vue
+++ src/pages/coaches/CoachRegestration.vue
@@ -1,3 +1,15 @@
 <template>
-  Register
+  <base-card>
+    <header>
+      <h2>Register as Coach</h2>
+    </header>
+    <coach-form></coach-form>
+  </base-card>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: { CoachForm }
+};
+</script>
